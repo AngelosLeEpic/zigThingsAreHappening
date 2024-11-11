0392c975f8179545414a1933e7c043fca447a251
CloudyUnity <finn.wright5@gmail.com>
2024-11-11 14:40:56 +0000

    DATA YEAGGH

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,55 @@
 const std = @import("std");
-const Math = std.math;
+const math = std.math;
+const rand = std.Random;
 
-pub fn main() void {
-    const rng = std.rand.DefaultPrng.init(123456789);
-    var p: point = point{ .x = rng.nextf64 % 1, .y = rng.nextf64 % 1 };
+const Point = struct { x: f64, y: f64 };
 
-    while (true) {
-        std.debug.print("{}\n{}\n", .{ p.x, p.y });
+pub fn main() !void {
+    var rng = rand.DefaultPrng.init(123456789);
+    var rando = rng.random();
+    var p = Point{ .x = rando.floatNorm(f64), .y = rando.floatNorm(f64) };
+
+    const currentWD = std.fs.cwd();
+    // std.debug.print("CWD: {}\n", .{currentWD.p});
+
+    const file = try currentWD.createFile("Data/TestNormal.csv", .{ .truncate = true });
+    const writer = file.writer();
+    try writer.print("Index, Value\n", .{});
+
+    const MAX_RUNS: c_int = 1;
+    for (0..MAX_RUNS) |i| {
+        //std.debug.print("px={}, py={}\n", .{ p.x, p.y });
         p = GetRandFromNormalDistribution(p, 0, 1);
+
+        try writer.print("{d}, {d}\n", .{ p.x, p.y });
+        //try writer.print("{d}, {d}\n", .{ i, p.x });
+        //try writer.print("{d}, {d}\n", .{ i, p.y });
+
+        std.debug.print("{x}\n", .{i});
     }
+
+    file.close();
 
     return;
 }
 
-const point = struct {
-    var x: f64 = 0;
-    var y: f64 = 0;
-};
+fn GetRandFromNormalDistribution(p: Point, mean: f64, stdDev: f64) Point {
+    const xI = math.floor(p.x);
+    const yI = math.floor(p.x);
 
-fn GetRandFromNormalDistribution(p: point, mean: f64, stdDev: f64) point {
-    const x: f64 = p.x % 1;
-    const y: f64 = p.y % 1;
+    const x: f64 = p.x - xI;
+    const y: f64 = p.y - yI;
 
     // https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
-    const z0 = std.math.sqrt(-2 * std.math.log(x)) * std.math.cos(2 * std.math.pi * y);
-    const z1 = std.math.sqrt(-2 * std.math.log(x)) * std.math.sin(2 * std.math.pi * y);
+    const logVal = math.log(f64, 10, x);
+    const mag = stdDev * math.sqrt(-2 * logVal);
+    const z0 = mag * math.cos(math.tau * y);
+    const z1 = mag * math.sin(math.tau * y);
 
-    return point{
-        .x = z0 * stdDev + mean,
-        .y = z1 * stdDev + mean,
+    //std.debug.print("RandNorm: x={d}, y={d}\n logVal={d}, mag={d}, z0={d}, z1={d}\n\n", .{ x, y, logVal, mag, z0, z1 });
+
+    return Point{
+        .x = z0 + mean,
+        .y = z1 + mean,
     };
 }
