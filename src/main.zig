ac41da0353bd8315f8eda395098b69bfb524a1f1
ThomasCreagh <github@thomascreagh.mailer.me>
2024-11-22 02:51:12 +0000

    finished the main dataframe functions and also the csv to dataframe :smile:

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,7 @@ pub fn main() !void {
 }
 
 fn Test_GetRandFromNormalDistribution() !void {
+    // writing data
     var rng = rand.DefaultPrng.init(123456789);
     var rando = rng.random();
     var p = global.Point{ .x = rando.floatNorm(f64), .y = rando.floatNorm(f64) };
@@ -18,16 +19,16 @@ fn Test_GetRandFromNormalDistribution() !void {
 
     const file = try currentWD.createFile("Data/TestNormal.csv", .{ .truncate = true });
     const writer = file.writer();
-    try writer.print("Index, Value\n", .{});
+    try writer.print("Index,Value\n", .{});
 
     const MAX_RUNS: c_int = 10000;
     for (0..MAX_RUNS) |i| {
         if (global.DEBUG_PRINT)
-            std.debug.print("px={}, py={}\n", .{ p.x, p.y });
-    
+            std.debug.print("px={},py={}\n", .{ p.x, p.y });
+
         p = dist.GetRandFromNormalDistribution(p, 0, 1);
 
-        try writer.print("{d}, {d}\n", .{ p.x, p.y });
+        try writer.print("{d},{d}\n", .{ p.x, p.y });
         //try writer.print("{d}, {d}\n", .{ i, p.x });
         //try writer.print("{d}, {d}\n", .{ i, p.y });
 
@@ -36,6 +37,22 @@ fn Test_GetRandFromNormalDistribution() !void {
     }
 
     file.close();
- 
+
+    // reading data
+    const zandas = @import("zandas.zig");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) std.testing.expect(false) catch @panic("TEST FAIL");
+    }
+    const allocator = gpa.allocator();
+
+    var df = try zandas.csv_to_df("Data/TestNormal.csv", allocator);
+    defer df.deinit();
+    const row = try df.get_row(0);
+    defer row.deinit();
+    std.debug.print("csv_data: {any}\n", .{row.items});
+    std.debug.print("csv_data: {any}\n", .{df.get(0, 0)});
+
     return;
 }
